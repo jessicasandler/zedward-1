1f97234161464b83e0994ec6775d385bcfb7bb44
Jessica Sandler <jasandle@asu.edu>
2022-12-01 14:41:21 -0700

    hello world

---
+++ helloworld.R
@@ -0,0 +1,11 @@
+#' Return a friendly message
+#'
+#' @param world to whom the message is directed
+#'
+#' @return A character vector containing to whom the message is directed
+#'
+#'
+#' @export
+hello_world <- function(name = "World") {
+  glue::glue("H")
+}
